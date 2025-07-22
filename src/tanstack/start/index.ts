9eab7e240882035d141de731763d09edc9180c7a
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-22 07:23:13 +0200

    chores(init): use tanstack query router as a model for init

--- src/tanstack/start/index.ts
+++ src/tanstack/start/index.ts
@@ -1,3 +1,3 @@
-export { createAgoraRouter, type AgoraRouterProps } from "./router";
+export { routerWithAgoraContext, type ValidateRouter, type AgoraRouterOptions } from "./router";
 export { createAgoraRootRoute, type RootRouteProps, type RouteContext } from "./root_route";
 export { useClientTags, type ClientManagedTagsContextType, useClientTag } from "./head_tags";
